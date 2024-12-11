0d7dea3417a4aeafb451a3cddae879917c37957b
brian <brian@ezeelink.co.id>
2024-12-11 19:40:33 +0700

    Setup a mock development and staging environment

--- EbuddyTask/ContentView.swift
+++ EbuddyTask/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    let buildNumber: String = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world! \(buildNumber)")
         }
         .padding()
     }
