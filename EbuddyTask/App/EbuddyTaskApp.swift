c7cf703eceddfe9a743762eb8e2deb79ca7d4040
brian <brian@ezeelink.co.id>
2024-12-11 18:39:54 +0700

    Firebase Setup. Firestore, Storage and Auth.

---
+++ EbuddyTask/App/EbuddyTaskApp.swift
@@ -0,0 +1,19 @@
+//
+//  EbuddyTaskApp.swift
+//  EbuddyTask
+//
+//  Created by brian on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EbuddyTaskApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
